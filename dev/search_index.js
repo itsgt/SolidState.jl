b26cef828d9363fe4fd4d89775dca1e72ddf2326
Documenter.jl <documenter@juliadocs.github.io>
2022-08-06 15:29:03 +0000

    build based on 3eda278

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SolidState","category":"page"},{"location":"#SolidState","page":"Home","title":"SolidState","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SolidState.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SolidState]","category":"page"}]
+}
